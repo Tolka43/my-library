f8807f0f2ef4c011b84930bcf71cdc02f50c8241
Ola Tomczyk <ola91057@gmail.com>
2021-01-17 11:15:09 +0100

    footer style

--- front/src/App.js
+++ front/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
 
       <Footer />
     </Router>
-  );
+  )
 };
 
 export default App;
--- front/src/Footer/Footer.js
+++ front/src/Footer/Footer.js
@@ -1,5 +1,20 @@
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagramSquare,
+  faLinkedin,
+  faFacebookSquare,
+} from '@fortawesome/free-brands-svg-icons';
 
-const Footer = () => <p id="footer">©Tolka43</p>;
+const Footer = () => (
+  <div className="footer">
+    <div className="icons-div">
+      <FontAwesomeIcon className="icon" icon={faInstagramSquare} />
+      <FontAwesomeIcon className="icon" icon={faFacebookSquare} />
+      <FontAwesomeIcon className="icon" icon={faLinkedin} />
+    </div>
+    <p className="copyright">© 2021 Tolka43</p>
+  </div>
+);
 
 export default Footer;
