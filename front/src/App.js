2996ff61ab741ddafa5f2a39861cc43c3d1f718e
Ola Tomczyk <ola91057@gmail.com>
2021-01-17 01:07:47 +0100

    footer

--- front/src/App.js
+++ front/src/App.js
@@ -4,28 +4,29 @@ import { useState } from 'react';
 import AppNavbar from './Navbar/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BookCreator from './BookCreator';
+import Footer from './Footer/Footer';
 
 const App = () => {
   const [books, setBooks] = useState();
 
   return (
-    <div className="App">
-      <Router>
-        <AppNavbar />
+    <Router>
+      <AppNavbar />
 
-        <Switch>
-          <Route path="/favorites"></Route>
-          <Route path="/settings"></Route>
-          <Route path="/">
-            <BookCreator />
-            <RefreshButton setBooks={setBooks} />
-            <div className="container-sm">
-              <Cards books={books} />
-            </div>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+      <Switch>
+        <Route path="/favorites"></Route>
+        <Route path="/settings"></Route>
+        <Route path="/">
+          <BookCreator />
+          <RefreshButton setBooks={setBooks} />
+          <div className="container-sm">
+            <Cards books={books} />
+          </div>
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Router>
   );
 };
 
---
+++ front/src/Footer/Footer.js
@@ -0,0 +1,5 @@
+import './Footer.css';
+
+const Footer = () => <p id="footer">©Tolka43</p>;
+
+export default Footer;
